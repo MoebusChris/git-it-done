5ce6e4bf59bf3c38623309236d8d0f9526bef15c
MoebusChris <suj022@ucsd.edu>
2021-11-16 23:26:21 -0800

    adding input block and buton

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,33 @@
 var getUserRepos = function(user) {
     //format the github api url
-    var apiUrl = "https://api.github.com/users/" + user +"/repos";
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     //make a request to the url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(response) {
+        response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
 getUserRepos();
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("PLease enter a Github username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
