1e039559c967216955cb2b77e72886a522605550
MoebusChris <suj022@ucsd.edu>
2021-11-21 10:29:24 -0800

    connecting two pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,10 @@
+//append <a> to the page
+var issueContainerEl = document.querySelector("#issues-container");
+
+var getRepoEl = 
+
+var queryString = document.location.search;
+
 var getRepoIssues = function(repo) {
     console.log(repo);
 
@@ -20,4 +27,37 @@ getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
 
+    for (var i = 0; i <issues.length; i++) {
+        //create a link element to take users to the issue on github.
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        //create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+        
+        //append to container
+        issueEl.appendChild(titleEl);
+
+        //create a type element
+        var typeEl = document.createElement("span");
+
+        //check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull Request)";
+        } else {
+            typeEl.textContent = "(Issue)"
+        }
+        ////append to container
+        issueEl.appendChild(typeEl);
+
+
+        issueContainerEl.appendChild(issueEl);
+    }
 };
+
+
+var repoName = queryString.split("=")[1];
+console.log(repoName);
